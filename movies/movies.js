90ee8a85a3b3a0f4f0427e458f0c0b016b5b36ff
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-01-17 20:14:26 -0500

    Working movie add delete

--- movies/movies.js
+++ movies/movies.js
@@ -1,9 +1,24 @@
 "use strict";
 
-$("#submit-movie-rating").on("click", handleSubmit);
 
+
+$("#submit-movie-rating").on("click", handleSubmit);
+$("ul").on("click", "button", handleDelete);
 function handleSubmit(evt) {
     evt.preventDefault();
-    let title = $("#title-input");
-    let rating = $("#rating-input");
+    let title = $("#title-input").val();
+    if(title.length < 2){
+        return;
+    }
+    let rating = $("#rating-input").val();
+
+    $("ul").append($(`<li>${title} ${rating} <button>Delete</button> </li>`));
+
+    $("#title-input").val("");
+    $("#rating-input").val("");
 }
+
+function handleDelete(evt) {
+    evt.currentTarget.parentNode.remove();
+}
+
