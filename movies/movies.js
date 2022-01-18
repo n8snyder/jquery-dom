f28934d1cea30bd191dbcfd2607e98e72089d6dd
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-17 16:38:18 -0800

    started movie ratings app

---
+++ movies/movies.js
@@ -0,0 +1,9 @@
+"use strict";
+
+$("#submit-movie-rating").on("click", handleSubmit);
+
+function handleSubmit(evt) {
+    evt.preventDefault();
+    let title = $("#title-input");
+    let rating = $("#rating-input");
+}
